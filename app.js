9a10a7ca09afb65e07c139e111f56c7876251698
anasgaya <anaasraheel.123@gmail.com>
2024-07-27 16:24:59 -0700

    complate

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert("this is wrong website");
+console.log('hello world')
+
+
+
+var chemistry=document.querySelector('#chemistry');
+var physics=document.querySelector('#physics');
+var maths=document.querySelector('#maths');
+var english=document.querySelector('#english');
+
+function result(){
+ if(chemistry.valu > 100 || physics.value > 100 || maths.value > 100 || english.value >100);
+   alert("marks are not correct");
+}
+
+function altResult(){
+var totalMarks =+chemistry.value + +physics.value + +maths.value + +english.value;
+var percentage = (totalMarks / 400) * 500;
+var grade;
+
+if(percentage > 80){
+    grade = 'A+';
+}
+
+}
